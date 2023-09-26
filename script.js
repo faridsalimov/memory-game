1c4618e76596e5fa18ce3a33fb36ce3a9238cb68
freezy-create <selimovferid85@gmail.com>
2023-09-27 00:24:27 +0400

    Upload.

---
+++ script.js
@@ -0,0 +1,86 @@
+const cards = document.querySelectorAll(".card");
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard = null;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("flipped");
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    checkCards();
+}
+
+function checkCards() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function checkWin() {
+    const matchedCards = document.querySelectorAll(".card.flipped");
+    if (matchedCards.length === cards.length) {
+        setTimeout(() => {
+            
+        }, 500);
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    checkWin();
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    hasFlippedCard = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+}
+
+const playAgainButton = document.getElementById("play-again-button");
+
+playAgainButton.addEventListener("click", function () {
+    resetGame();
+    document.getElementById("win-overlay").classList.remove("show");
+    document.getElementById("win-overlay").style.zIndex = -1;
+});
+
+function resetGame() {
+    randomCards();
+    cards.forEach(card => card.classList.remove("flipped"));
+    resetBoard();
+}
+
+function randomCards() {
+    cards.forEach(card => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    });
+}
+
+document.getElementById("win-overlay").classList.add("show");
+document.getElementById("win-overlay").style.zIndex = 1;
+
+randomCards();
+cards.forEach(card => card.addEventListener("click", flipCard));
